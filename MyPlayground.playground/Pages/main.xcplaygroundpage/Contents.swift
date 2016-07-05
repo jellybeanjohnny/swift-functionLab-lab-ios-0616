c725e7836cc7369eca500da2b94399f52a3b94b2
jellybeanjohnny <mamerige@icloud.com>
2016-07-05 15:09:29 -0400

    answered all questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,8 +12,11 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWord() {
+	print("Hello, World")
+}
 
-
+helloWord()
 
 
 /*: question2
@@ -21,14 +24,25 @@
  */
 // write your code here
 
+func printConstant() {
+	let bark = "BARK"
+	print(bark)
+}
 
-
+printConstant()
 
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
+func greet(name: String) {
+	print("Hello, \(name)")
+}
+
+greet("Sir Berekely")
+greet("Jeff")
+greet("Lee Harvey Oswald")
 
 
 
@@ -36,7 +50,14 @@
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+let bulbasaur = "bulbasaur"
+greet(bulbasaur)
 
+var charmandar = "charmandar"
+greet(charmandar)
+
+charmandar = "Robert"
+greet(charmandar)
 
 
 
@@ -44,8 +65,17 @@
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func theMagicMan() {
+	var swisscheese = "swiss cheese"
+	print(swisscheese)
+	swisscheese = "cheddar"
+	print(swisscheese)
 
+}
 
+theMagicMan()
+theMagicMan()
+theMagicMan()
 
 
 /*:
